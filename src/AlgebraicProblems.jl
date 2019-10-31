21c1f6a54d7d6b9c10705f183cceeb7feeb86d29
David AW Barton <david.barton@bristol.ac.uk>
2019-10-31 22:41:04 +0000

    Added docstrings for AlgebraicProblems

--- src/AlgebraicProblems.jl
+++ src/AlgebraicProblems.jl
@@ -1,16 +1,24 @@
 """
     AlgebraicProblems
 
-This module implements basic functionality to continue algebraic problems of the
-form
+This module implements basic functionality to construct algebraic problems of
+the form
 
 ```math
     0 = f(u, p)
 ```
+
+where `u` and `p` are the state variables and parameters respectively. Both `u`
+and `p` can be scalars or vectors.
+
+See [`addalgebraicproblem!`](@ref) for details.
 """
 module AlgebraicProblems
 
-using ..ProblemStructures:ProblemStructure, addvar!, addfunc!, addpars!
+using DocStringExtensions
+using ..ProblemStructures: ProblemStructure, addvar!, addfunc!, addpars!
+
+export addalgebraicproblem!
 
 struct AlgebraicProblem{U, P, F}
     f!::F
@@ -21,6 +29,37 @@ _convertto(::Type{<:Number}, val) = val[1]
 
 (ap::AlgebraicProblem{U, P})(res, u, p) where {U, P} = ap.f!(res, _convertto(U, u), _convertto(P, p))
 
+"""
+$SIGNATURES
+
+Construct an algebraic zero problem of the form 
+
+```math
+    0 = f(u, p),
+```
+
+where `u` is the state and `p` is the parameter(s), and add it to the problem
+structure. The function can operate on scalars or vectors, and be in-place or
+not. It assumes that the function output is of the same dimension as `u`.
+
+# Parameters
+
+* `prob::ProblemStructure` : the underlying continuation problem.
+* `name::String` : the name of the algebraic zero problem.
+* `f` : the function to use for the zero problem. It takes either two arguments
+  (`u` and `p`) or three arguments for an in-place version (`res`, `u`, and `p`).
+* `u0` : the initial state value (either scalar- or vector-like).
+* `p0` : the initial parameter value (either scalar- or vector-like).
+* `pnames` : (keyword, optional) the names of the parameters. If not specified,
+  auto-generated names will be used.
+
+# Example
+
+```
+prob = ProblemStructure()
+addalgebraicproblem!(prob, "cubic", (u, p) -> u^3 - p, 1.5, 1)  # u0 = 1.5, p0 = 1
+```
+"""
 function addalgebraicproblem!(prob::ProblemStructure, name::String, f, u0, p0; pnames=nothing)
     # Determine whether f is in-place or not
     if any(method.nargs == 4 for method in methods(f))
