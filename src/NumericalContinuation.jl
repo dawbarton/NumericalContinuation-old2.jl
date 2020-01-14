fa4bd8c4780a4ffc235592433446104fb2c7dc40
David AW Barton <david.barton@bristol.ac.uk>
2020-01-14 14:15:10 +0000

    Another rename

--- src/NumericalContinuation.jl
+++ src/NumericalContinuation.jl
@@ -1,6 +1,9 @@
 module NumericalContinuation
 
-include("problem_structures.jl")
+include("continuation_functions.jl")
+include("monitor_functions.jl")
+
+
 
 # include("AlgebraicProblems.jl")
 # include("ShootingProblems.jl")
