2157e62374dd995a06ff9a5989757b5a998e2580
David AW Barton <david.barton@bristol.ac.uk>
2020-01-24 17:52:31 +0000

    Test file

---
+++ test/test_SimpleCovering.jl
@@ -0,0 +1,7 @@
+using NumericalContinuation.AlgebraicProblems
+
+@testset "SimpleCovering" begin
+    prob = ProblemStructure()
+    add_algebraicproblem!(prob, "cubic", (u, p) -> u^3 - u - p, 1.5, 1, pnames=["p"])
+    bifn = continuation(prob, "p"=>[-2,2])
+end
